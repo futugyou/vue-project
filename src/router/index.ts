742fbc9e30a3d8b00f4efc5e7f7d467cd0ca84ba
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-15 20:17:24 +0800

    add count page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import Home from '../components/HelloWorld.vue'
+import Count from '../components/Count.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/count',
+    name: 'Count',
+    component: Count
   } 
 ]
 
