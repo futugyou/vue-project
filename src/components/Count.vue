801d2a8517868c2930c7ce42dd78aa75c7e4e3cf
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-15 20:30:16 +0800

    add event listener

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -5,15 +5,20 @@ const counter = reactive({ count: 0 })
 const message = ref('Hello World!')
 
 const pClass = ref('title')
+const increment = () => {
+    counter.count++
+    message.value="Count has been increment"
+}
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
-  <p :class="pClass">Count is: {{ counter.count }}</p>
+    <h1>{{ message }}</h1>
+    <p :class="pClass">Count is: {{ counter.count }}</p>
+    <button @click="increment">Increment Count</button>
 </template>
 
 <style>
 .title {
-  color: red;
+    color: red;
 }
 </style>
