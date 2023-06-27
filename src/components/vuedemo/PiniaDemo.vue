74351e198cf0be60d6c174d36f48b72cb315ba07
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-27 19:31:59 +0800

    pinia, use subscribe

--- src/components/vuedemo/PiniaDemo.vue
+++ src/components/vuedemo/PiniaDemo.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import { useCounterStore } from '../../stores/counter'
+import { watch } from 'vue'
 import { storeToRefs } from 'pinia'
+
 // 可以在组件中的任意位置访问 `store` 变量 ✨
 const store = useCounterStore()
 const { count } = storeToRefs(store)
@@ -9,12 +11,26 @@ const incrementClick = () => {
     store.count++
 }
 
-const reset=()=>{
+const reset = () => {
     store.$reset()
 }
 
-</script>
+const change = () => {
+    store.$state = { count: 24 }
 
+    // 在它内部调用 `$patch()`：
+    //store.$patch({ count: 24 })
+}
+
+store.$subscribe((mutation, state) => {
+    // 'direct' | 'patch object' | 'patch function'
+    console.log(mutation.type)
+    console.log(mutation.storeId)
+
+    localStorage.setItem('counter', JSON.stringify(state))
+})
+
+</script>
 
 <template>
     <div class="vueapp-container">
@@ -33,6 +49,10 @@ const reset=()=>{
         <div class="vueapp-layer">
             <h3>{{ count }}</h3>
             <button @click="reset">reset</button>
+        </div>
+        <div class="vueapp-layer">
+            <h3>{{ count }}</h3>
+            <button @click="change">change</button>
         </div>
     </div>
 </template>
