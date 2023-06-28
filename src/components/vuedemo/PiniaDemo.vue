0a39ddddb46b45437c1a8e45a6f9054b04630ccd
futugyou <17871902+futugyou@users.noreply.github.com>
2023-06-28 17:00:42 +0800

    pinia, add store action subscribe

--- src/components/vuedemo/PiniaDemo.vue
+++ src/components/vuedemo/PiniaDemo.vue
@@ -6,6 +6,38 @@ import { State } from 'xstate';
 
 // 可以在组件中的任意位置访问 `store` 变量 ✨
 const store = useCounterStore()
+const unsubscribe = store.$onAction(
+  ({
+    name, // action 名称
+    store, // store 实例，类似 `someStore`
+    args, // 传递给 action 的参数数组
+    after, // 在 action 返回或解决后的钩子
+    onError, // action 抛出或拒绝的钩子
+  }) => {
+    // 为这个特定的 action 调用提供一个共享变量
+    const startTime = Date.now()
+    // 这将在执行 "store "的 action 之前触发。
+    console.log(`Start "${name}" with params [${args.join(', ')}].`)
+
+    // 这将在 action 成功并完全运行后触发。
+    // 它等待着任何返回的 promise
+    after((result) => {
+      console.log(
+        `Finished "${name}" after ${
+          Date.now() - startTime
+        }ms.\nResult: ${result}.`
+      )
+    })
+
+    // 如果 action 抛出或返回一个拒绝的 promise，这将触发
+    onError((error) => {
+      console.warn(
+        `Failed "${name}" after ${Date.now() - startTime}ms.\nError: ${error}.`
+      )
+    })
+  }
+)
+
 const { count } = storeToRefs(store)
 const { increment } = store
 const incrementClick = () => {
