1e66012458c620ff2b262f0697bdb7a07c035890
futugyou <17871902+futugyou@users.noreply.github.com>
2023-07-06 19:54:46 +0800

    get account data from server

--- src/components/aws/AccountDetail.vue
+++ src/components/aws/AccountDetail.vue
@@ -2,14 +2,42 @@
 import { ref, watchEffect, computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
+import Spinners from '../Spinners.vue'
+
+interface AccountDetail {
+    id: string
+    alias: string
+    accessKeyId: string
+    secretAccessKey: string
+    region: string
+    createdAt: number
+}
+
 const route = useRoute()
+const isLoading = ref(true)
+const account = ref<AccountDetail | null>(null)
+
 const accountId = computed(() => route.params.accountId)
+const accountDetailEndpoint = computed(() => import.meta.env.REACT_APP_AWS_SERVER + 'v1/accounts/' + accountId.value)
+
+const fetchData = async () => {
+    isLoading.value = true
+    const res = await fetch(accountDetailEndpoint.value)
+    account.value = await res.json()
+    isLoading.value = false
+}
+
+watchEffect(async () => fetchData())
 
 </script>
 
 <template>
     <div class="full-content">
-        this account id is {{ accountId }}
+        <Spinners v-if="isLoading"></Spinners>
+        <div v-if="!isLoading">
+            this account is {{ account }}
+        </div>
+
     </div>
 </template>
 
