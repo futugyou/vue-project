bd288949768cc2f5b9b8d171d8953c9f1eb9992c
futugyou <17871902+futugyou@users.noreply.github.com>
2023-08-26 15:16:18 +0800

    add ecs serivce list page

---
+++ src/components/aws/ecs/EcsService.vue
@@ -0,0 +1,129 @@
+<script lang="ts" setup>
+import { ref, watchEffect } from 'vue'
+import { useRouter } from 'vue-router'
+
+import TableAndPaging, { TableField } from '@/components/TableAndPaging.vue'
+import { EcsService, getEcsServices } from "./ecs";
+
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+const store = useMessageStore()
+const { msg } = storeToRefs(store)
+
+const router = useRouter()
+const isLoading = ref(true)
+const limit = ref(30)
+const page = ref(1)
+const ecsServices = ref<EcsService[]>([])
+
+const account = ref('')
+
+
+
+const fields: TableField[] = [
+    {
+        key: 'id',
+        label: '#',
+        header: true
+    },
+    {
+        key: 'account_alias',
+        label: 'Account Alias'
+    },
+    {
+        key: 'cluster_name',
+        label: 'Cluster Name'
+    },
+    {
+        key: 'service_name',
+        label: 'Service Name'
+    },
+    {
+        key: 'operate_At',
+        label: 'Operate At'
+    },
+    {
+        key: 'operation',
+        label: 'Operation'
+    }
+]
+
+const fetchData = async () => {
+    isLoading.value = true
+    const { data, error } = await getEcsServices(page.value, limit.value, account.value)
+    if (error) {
+        msg.value = {
+            errorMessages: [error.message],
+            delay: 3000,
+        }
+        return
+    }
+
+    ecsServices.value = data ?? []
+    isLoading.value = false
+}
+
+watchEffect(async () => fetchData())
+
+const updatePage = (n: number) => {
+    page.value = n
+}
+
+const changePagesize = (n: number) => {
+    limit.value = n
+}
+
+
+</script>
+
+<template>
+    <div class="ecs-full-content">
+        <div class="head-content">
+            <div class="">
+                <h1>ecs service</h1>
+            </div>
+        </div>
+        <TableAndPaging :items="ecsServices" :fields="fields" :isLoading="isLoading" @changePagesize="changePagesize"
+            @updatePage="updatePage">
+            <template v-slot:header_id="header">
+                <span style="color: red">{{ header.label }}</span>
+            </template>
+            <template v-slot:body_id="body">
+                <router-link :to="'/ecsservice/' + body.id" page-path="" class="detail-link">
+                    <span>
+                        {{ body.id }}
+                    </span>
+                </router-link>
+            </template>
+            <template v-slot:body_operation="body">
+            </template>
+        </TableAndPaging>
+    </div>
+</template>
+
+<style scoped>
+.ecs-full-content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    position: relative;
+}
+
+.head-content {
+    display: flex;
+    align-items: center;
+    padding: 0px 10px;
+    justify-content: space-between;
+}
+
+.detail-link {
+    width: 100%;
+    height: 100%;
+    display: block;
+}
+
+.gap-right-10 {
+    margin-right: 10px;
+}
+</style>
