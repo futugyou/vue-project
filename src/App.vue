eabb7c7fecfcaba0222b1f7976e24be8037051c5
futugyou <17871902+futugyou@users.noreply.github.com>
2023-11-10 18:48:53 +0800

    add check when app in sub mirco-app env

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,72 @@
 <script setup lang="ts">
-import { shallowRef } from 'vue'
+import { ref, shallowRef, watchEffect } from 'vue'
 import DemoRoute from './components/vuedemo/DemoRoute.vue'
 import AwsRoute from './components/aws/AwsRoute.vue'
 import TabContainer from './components/TabContainer.vue'
 import Alert from '@/components/Alert.vue'
 
 import { handleGlobalData } from '@/tools/baseAppEvent'
-
+const rootContainer = ref("nomalRootContainer")
 const onRouteChange = () => {
     handleGlobalData()
 }
+
+watchEffect(() => {
+    if (window.__MICRO_APP_ENVIRONMENT__) {
+        rootContainer.value = "subAppRootContainer"
+    } else {
+        rootContainer.value = "nomalRootContainer"
+    }
+})
 
 const components = shallowRef([AwsRoute, DemoRoute] as unknown as [])
 </script>
 
 <template>
     <Alert></Alert>
-    <div id="public-links" @click="onRouteChange">
-        <KeepAlive>
-            <TabContainer :components="components"> </TabContainer>
-        </KeepAlive>
-    </div>
-    <div class="vu3-app">
-        <router-view v-slot="{ Component, route }">
-            <!-- 使用任何自定义过渡和回退到 `fade` -->
-            <transition :name="route.meta.transition as string || 'fade'">
-                <KeepAlive>
-                    <component :is="Component" :key="route.path" />
-                </KeepAlive>
-            </transition>
-        </router-view>
+    <div :class="rootContainer">
+        <div id="public-links" @click="onRouteChange">
+            <KeepAlive>
+                <TabContainer :components="components" v-if="rootContainer == 'nomalRootContainer'"> </TabContainer>
+            </KeepAlive>
+        </div>
+        <div class="vu3-app">
+            <router-view v-slot="{ Component, route }">
+                <!-- 使用任何自定义过渡和回退到 `fade` -->
+                <transition :name="route.meta.transition as string || 'fade'">
+                    <KeepAlive>
+                        <component :is="Component" :key="route.path" />
+                    </KeepAlive>
+                </transition>
+            </router-view>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+#public-links {
+    padding: 10px 0;
+    font-size: 18px;
+}
+.vu3-app {
+    flex-grow: 1;
+    height: 100%;
+    width: 100%;
+    overflow-y: auto;
+}
+
+.nomalRootContainer {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.subAppRootContainer {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+}
+</style>
