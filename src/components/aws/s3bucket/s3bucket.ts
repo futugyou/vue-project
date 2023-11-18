337cf5219fba0cc44cde8a70bd2e5deb813043ec
futugyou <17871902+futugyou@users.noreply.github.com>
2023-11-18 14:58:45 +0800

    add s3bucket api client

---
+++ src/components/aws/s3bucket/s3bucket.ts
@@ -0,0 +1,69 @@
+import { fetchEx } from '@/tools/fetch'
+
+export interface S3Bucket {
+    id: string
+    accountId: string
+    accountName: string
+    name: string
+    region: string
+    isPublic: boolean
+    policy: string
+    permissions: string[]
+    creationDate: string
+}
+
+export const defaultS3Bucket: S3Bucket = {
+    id: '',
+    accountId: '',
+    accountName: '',
+    name: '',
+    region: '',
+    isPublic: false,
+    policy: '',
+    permissions: [],
+    creationDate: ''
+}
+
+export const getS3Buckets = async (page: number, limit: number, bucketName: string) => {
+    let parameterGetEndpoint =
+        import.meta.env.REACT_APP_AWS_SERVER + 'v1/s3?page=' + page + '&limit=' + limit
+    if (bucketName) {
+        parameterGetEndpoint += '&bucketName=' + bucketName
+    }
+
+    return fetchEx(parameterGetEndpoint, ...[, ,], true)
+}
+
+export interface S3BucketItem {
+    id: string
+    bucketName: string
+    key: string
+    size: number
+    isDirectory: boolean
+    creationDate: string
+}
+
+export const defaultS3BucketItem: S3BucketItem = {
+    id: '',
+    bucketName: '',
+    key: '',
+    size: 0,
+    isDirectory: false,
+    creationDate: ''
+}
+
+export const getS3BucketItems = async (page: number, limit: number, bucketName: string, accountId: string, perfix: string) => {
+    let parameterGetEndpoint =
+        import.meta.env.REACT_APP_AWS_SERVER + 'v1/s3/items?page=' + page + '&limit=' + limit
+    if (bucketName) {
+        parameterGetEndpoint += '&bucketName=' + bucketName
+    }
+    if (accountId) {
+        parameterGetEndpoint += '&accountId=' + accountId
+    }
+    if (perfix) {
+        parameterGetEndpoint += '&perfix=' + perfix
+    }
+
+    return fetchEx(parameterGetEndpoint, ...[, ,], true)
+}
