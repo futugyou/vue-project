a28155a45fa19c255cf4f1a3616f39d543a63f1c
futugyou <17871902+futugyou@users.noreply.github.com>
2023-11-27 20:27:47 +0800

    open new window icon

---
+++ src/components/icons/Open.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <span class="icon-base">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" focusable="false" aria-hidden="true">
+            <path class="stroke-linecap-square" d="M10 2h4v4"></path>
+            <path d="m6 10 8-8"></path>
+            <path class="stroke-linejoin-round" d="M14 9.048V14H2V2h5"></path>
+        </svg>
+    </span>
+</template>
