60a76c4e4d2d476c5619c5c5ab7f26fdf1c0b61d
futugyou <17871902+futugyou@users.noreply.github.com>
2023-11-27 21:44:04 +0800

    update spinner component

--- src/components/Spinners.vue
+++ src/components/Spinners.vue
@@ -1,5 +1,25 @@
+<script lang="ts" setup>
+import { reactive } from "vue"
+const props = defineProps<{
+    width?: string,
+    height?: string,
+}>()
+
+const theme = reactive({
+    width: props.width ?? '3rem',
+    height: props.height ?? '3rem',
+})
+
+</script>
+
 <template>
-    <div class="spinner-border text-info" style="width: 3rem; height: 3rem" role="status">
+    <div class="spinner-border text-info spinner-size" role="status">
         <span class="visually-hidden">Loading...</span>
     </div>
-</template>
+</template> 
+<style scoped>
+.spinner-size {
+    width: v-bind('theme.width');
+    height: v-bind('theme.height');
+}
+</style>
