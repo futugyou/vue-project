14ee81bee3cee4453ffb256c7856c6ebe9567e01
futugyou <17871902+futugyou@users.noreply.github.com>
2023-11-28 19:11:05 +0800

    translate env

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly REACT_APP_AWS_SERVER: string 
+    readonly REACT_APP_AWS_SERVER: string
+    readonly REACT_APP_TRANSLATE_SERVER: string
+    readonly REACT_APP_TRANSLATE_KEY: string
+    readonly REACT_APP_TRANSLATE_REGION: string
 }
 
 interface ImportMeta {
