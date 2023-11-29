9050301e6d1e579e6a550185840038159157eff7
futugyou <17871902+futugyou@users.noreply.github.com>
2023-11-29 14:09:13 +0800

    fix 'Top-level await is not available'

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,12 @@ export default defineConfig({
       },
     }),
     vueJsx(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
+    }),
   ],
   resolve: {
     alias: {
