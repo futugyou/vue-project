2e80b89ce731e6d7c25fe9ce1377ca1c8e41c63b
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-04 15:58:43 +0800

    add translate page

--- src/components/vuedemo/DPFViewerDemo.vue
+++ src/components/vuedemo/DPFViewerDemo.vue
@@ -3,12 +3,64 @@
 import { ref, PropType, computed } from 'vue'
 
 import DPFViewer from '@/components/DPFViewer.vue'
+import Button from '@/components/Button.vue'
 
+const right = ref("")
+const showTranslate = ref(false)
 </script>
 
 <template>
-    <DPFViewer>
-    </DPFViewer>
+    <div class="viewer-content">
+        <div class="viewer-container">
+            <DPFViewer>
+            </DPFViewer>
+        </div>
+        <div class="tran-contr">
+            <Button Text="O" @click="showTranslate = !showTranslate"> </Button>
+        </div>
+        <div class="translate-container" v-if="showTranslate">
+            <div>
+                <Button Text="Translate">
+                </Button>
+            </div>
+            <div class="text-container">
+                <textarea v-model="right" placeholder=""></textarea>
+            </div>
+        </div>
+    </div>
 </template>
   
-<style></style>
+<style scoped>
+.viewer-content {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    grid-gap: 5px;
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+    flex-direction: row;
+    position: relative;
+}
+
+.viewer-container {
+    flex: 2;
+}
+
+.tran-contr {
+    position: absolute;
+    right: 10px;
+    top: 0;
+}
+
+.translate-container {
+    display: flex;
+    flex-direction: column;
+    grid-gap: 10px;
+    flex: 1;
+}
+
+.text-container {
+    flex-grow: 1;
+}
+</style>
