ed1910d6dab05f550b46504e532c79480e97ecee
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-07 20:55:17 +0800

    update file input component

--- src/common/FileInput.vue
+++ src/common/FileInput.vue
@@ -7,6 +7,7 @@ const props = defineProps<{
     loading?: boolean
 }>()
 
+const labeltext = ref("choose file")
 const emit = defineEmits<{
     (e: 'fileLoad', f: File): void
 }>()
@@ -14,12 +15,18 @@ const emit = defineEmits<{
 const onFileChange = (event: Event) => {
     const target = event.target as HTMLInputElement
     if (target == null || target.files == null || target.files.length == 0 || !target.files[0]) {
+        labeltext.value = "choose file"
         return
     }
 
     const file = target.files[0]
+    labeltext.value = file.name
     emit('fileLoad', file)
 }
+
+onMounted(() => {
+    labeltext.value = "choose file"
+})
 
 </script>
 
@@ -27,7 +34,9 @@ const onFileChange = (event: Event) => {
     <div class="file-container">
         <div style="flex:1">
             <form style="width: 100%;">
-                <input type="file" @change="onFileChange" :disabled="loading" />
+                <label for="file-reader" class="file-reader-label">{{ labeltext }}</label>
+                <input id="file-reader" type="file" name="file-reader" class="file-reader-input" @change="onFileChange"
+                    :disabled="loading" />
             </form>
         </div>
         <div style="display: flex;align-items: center;padding-left: 10px;">
@@ -42,5 +51,36 @@ const onFileChange = (event: Event) => {
     flex-direction: row;
     width: 100%;
     grid-gap: var(--grid-gap-10);
+}
+
+.file-reader-label {
+    font-size: var(--font-size-body-m);
+    border: 1px solid var(--color-border-text-normal-default);
+    border-radius: var(--border-radius-input);
+    padding-block: var(--space-scaled-xxs);
+    padding-inline: var(--space-button-horizontal);
+    line-height: 16px;
+    white-space: nowrap;
+    height: 36px;
+    letter-spacing: .25px;
+    align-items: center;
+    text-align: left;
+    box-sizing: border-box;
+    color: var(--color-border-text-normal-default);
+    cursor: pointer;
+    display: inline-flex;
+    font-family: "Google Sans", sans-serif;
+    font-weight: 500;
+    justify-content: flex-start;
+    width: 100%;
+}
+
+.file-reader-input {
+    height: 0.1px;
+    opacity: 0;
+    overflow: hidden;
+    position: absolute;
+    width: 0.1px;
+    z-index: -1;
 }
 </style>
