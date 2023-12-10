995fbca9759b14cc45ecbd16deb4e22e3f9a2b07
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-10 19:33:02 +0800

    split node modules chunks

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,16 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 export default defineConfig({
   envPrefix: "REACT_APP_",
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    }
   },
   plugins: [
     vue({
