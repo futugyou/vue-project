f116e225a2c24e3d231bd3bf511a754c7ebb2a06
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-12 18:34:22 +0800

    init dropdown component

---
+++ src/common/Dropdown.vue
@@ -0,0 +1,99 @@
+<script lang="ts" setup>
+import { defineComponent, ref } from 'vue'
+import Up from '@/icons/Up.vue'
+import Down from '@/icons/Down.vue'
+
+export interface IDropdownProps {
+    items: Record<string, string>
+    defaultValue?: string
+}
+
+const props = defineProps<IDropdownProps>()
+const emit = defineEmits<{
+    (e: 'changeSelected', n: string): void
+}>()
+
+const selectedValue = ref(props.defaultValue ?? "---")
+
+const showList = ref(false)
+const changeSelected = (key: string) => {
+    emit('changeSelected', key)
+    const selectItem = props.items[key]
+    if (selectItem) {
+        selectedValue.value = selectItem
+    } else {
+        selectedValue.value = ""
+    }
+
+    showList.value = false
+}
+</script>
+
+<template>
+    <div class="dropdown-container">
+        <div>
+            <button type="button" class="dropdown-btn" @click="showList = !showList">
+                <span>{{ selectedValue }}</span>
+
+                <Up v-if="showList"></Up>
+                <Down v-if="!showList"></Down>
+            </button>
+        </div>
+
+        <div class="dropdown-items-container">
+            <ul v-if="showList">
+                <li v-for="(value, key) in items" :key="key" @click="changeSelected(key)">{{ value }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.dropdown-container {
+    position: relative;
+    white-space: nowrap;
+    width: 100%;
+}
+
+.dropdown-items-container {
+    position: absolute;
+    width: 100%;
+    text-align: left;
+    line-height: 30px;
+    margin-top: 5px;
+}
+
+.dropdown-btn {
+    border: 1px solid var(--color-border-button-normal-default);
+    border-radius: 10px;
+    padding: 0px 10px;
+    width: 100%;
+    height: 40px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+}
+
+.dropdown-btn svg {
+    width: 20px;
+    height: 20px;
+}
+
+.dropdown-items-container ul {
+    border: 1px solid var(--color-border-button-normal-default);
+    border-radius: 10px;
+    padding: 0px 10px;
+}
+
+.dropdown-items-container * li {
+    cursor: pointer;
+}
+
+
+.dropdown-items-container * li:hover {
+    background-color: var(--color-background-dropdown-normal-hover);
+}
+</style>
