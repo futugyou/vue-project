54341c2af8d272151ff75dd00a84a0b9c653c73a
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-13 21:01:26 +0800

    dictionary lookup api

--- src/components/tools/translate/Translate.ts
+++ src/components/tools/translate/Translate.ts
@@ -11,10 +11,33 @@ export interface DetectLanguageModel {
     isTransliterationSupported: boolean
 }
 
+export interface DictionaryLookupModel {
+    normalizedSource: string
+    displaySource: string
+    translations: Translation[]
+}
+
+export interface Translation {
+    normalizedTarget: string
+    displayTarget: string
+    posTag: string
+    confidence: number
+    prefixWord: string
+    backTranslations: BackTranslation[]
+}
+
+export interface BackTranslation {
+    normalizedText: string
+    displayText: string
+    numExamples: number
+    frequencyCount: number
+}
+
 const api_version = "api-version=" + import.meta.env.REACT_APP_TRANSLATE_VSERION
 const api_endpoint = import.meta.env.REACT_APP_TRANSLATE_SERVER
 const translate_api = api_endpoint + 'translate?' + api_version
 const detect_api = api_endpoint + 'detect?' + api_version
+const lookup_api = api_endpoint + 'dictionary/lookup?' + api_version
 
 export const translateText = async (from: string, to: string, model: TranslateModel[]) => {
     const translateEndpoint = translate_api + '&from=' + from + '&to=' + to
@@ -34,4 +57,14 @@ export const detectLanguage = async (from: string, to: string, model: TranslateM
     }
 
     return fetchEx(detectEndpoint, 'post', model, false, h)
+}
+
+export const lookupDictionary = async (from: string, to: string, model: TranslateModel[]) => {
+    const lookupEndpoint = lookup_api + '&from=' + from + '&to=' + to
+    let h: Record<string, string> = {
+        'Ocp-Apim-Subscription-Key': import.meta.env.REACT_APP_TRANSLATE_KEY,
+        'Ocp-Apim-Subscription-Region': import.meta.env.REACT_APP_TRANSLATE_REGION,
+    }
+
+    return fetchEx(lookupEndpoint, 'post', model, false, h)
 }
