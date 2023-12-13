1c939d006a034cf90627b8425e5f55f3c6d0f447
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-13 20:01:05 +0800

    add translate detect api

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
     readonly REACT_APP_AWS_SERVER: string
     readonly REACT_APP_TRANSLATE_SERVER: string
+    readonly REACT_APP_TRANSLATE_VSERION: string
     readonly REACT_APP_TRANSLATE_KEY: string
     readonly REACT_APP_TRANSLATE_REGION: string
     readonly REACT_APP_PDFJS_CDN: string
--- src/components/tools/translate/Translate.ts
+++ src/components/tools/translate/Translate.ts
@@ -4,12 +4,34 @@ export interface TranslateModel {
     Text: string
 }
 
+export interface DetectLanguageModel {
+    language: string
+    score: number
+    isTranslationSupported: boolean
+    isTransliterationSupported: boolean
+}
+
+const api_version = "api-version=" + import.meta.env.REACT_APP_TRANSLATE_VSERION
+const api_endpoint = import.meta.env.REACT_APP_TRANSLATE_SERVER
+const translate_api = api_endpoint + 'translate?' + api_version
+const detect_api = api_endpoint + 'detect?' + api_version
+
 export const translateText = async (from: string, to: string, model: TranslateModel[]) => {
-    const translateEndpoint = import.meta.env.REACT_APP_TRANSLATE_SERVER + '&from=' + from + '&to=' + to
+    const translateEndpoint = translate_api + '&from=' + from + '&to=' + to
     let h: Record<string, string> = {
         'Ocp-Apim-Subscription-Key': import.meta.env.REACT_APP_TRANSLATE_KEY,
         'Ocp-Apim-Subscription-Region': import.meta.env.REACT_APP_TRANSLATE_REGION,
     }
 
     return fetchEx(translateEndpoint, 'post', model, false, h)
+}
+
+export const detectLanguage = async (from: string, to: string, model: TranslateModel[]) => {
+    const detectEndpoint = detect_api
+    let h: Record<string, string> = {
+        'Ocp-Apim-Subscription-Key': import.meta.env.REACT_APP_TRANSLATE_KEY,
+        'Ocp-Apim-Subscription-Region': import.meta.env.REACT_APP_TRANSLATE_REGION,
+    }
+
+    return fetchEx(detectEndpoint, 'post', model, false, h)
 }
