82da2eb6b6c7ebcbfe0cc560ebeb3635043a4581
futugyou <17871902+futugyou@users.noreply.github.com>
2023-12-16 21:41:23 +0800

    speech component

---
+++ src/components/tools/translate/ItemWithSpeech.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+import Speech from '@/common/Speech.vue'
+
+export interface IItemWithSpeechProps {
+    lang: string,
+    text: string,
+}
+
+const props = withDefaults(defineProps<IItemWithSpeechProps>(), {
+    lang: 'en',
+    text: '',
+})
+
+</script>
+
+<template>
+    <div class="text-container">
+        <div>
+            <slot></slot>
+        </div>
+        <div class="speech-btn">
+            <Speech :lang="lang" :text="text"></Speech>
+        </div>
+    </div>
+</template>
+
+<style scoped> .text-container {
+     display: flex;
+     grid-gap: var(--grid-gap-10);
+     align-items: center;
+     cursor: default
+ }
+
+ .speech-btn {
+     display: none;
+ }
+
+ .text-container:hover>.speech-btn {
+     display: block;
+ }
+</style>
