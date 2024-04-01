52bda10f25ab51887500a42b9d3de46025287ad9
futugyou <17871902+futugyou@users.noreply.github.com>
2024-04-01 15:58:31 +0800

    add image svg

---
+++ src/icons/ImageSvg.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+export interface ImageSvgProps {
+    width?: string
+    height?: string
+    href: string
+    title: string
+}
+
+const props = withDefaults(defineProps<ImageSvgProps>(), {
+    width: "30",
+    height: "30"
+})
+
+</script>
+
+<template>
+    <svg :width="props.width" :height="props.height" xmlns="http://www.w3.org/2000/svg">
+        <title v-if="props.title">{{ props.title }}</title>
+        <image :href="props.href" :width="props.width" :height="props.height" />
+    </svg>
+</template>
