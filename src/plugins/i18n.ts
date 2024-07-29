c80afb5536defa0963e583f8138bb88712004d9a
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-07-29 10:11:10 +0000

    fix ts error

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,18 +1,8 @@
 import { i18nKey } from '../tools/injectkey'
 
-declare module 'vue' {
-    interface ComponentCustomProperties {
-        $translate: (key: string) => string
-    }
-}
-
 export default {
     install: (app: any, options: any) => {
-        // 注入一个全局可用的 $translate() 方法
         app.config.globalProperties.$translate = (key: string) => {
-            // 获取 `options` 对象的深层属性
-            // 使用 `key` 作为索引
-            console.log(options)
             return key.split('.').reduce((o, i) => {
                 if (o) return o[i]
             }, options)
