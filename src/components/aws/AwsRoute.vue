886d089fe71ce6ce8ea26f5c7b7154500750662a
futugyou <17871902+futugyou@users.noreply.github.com>
2024-08-17 00:15:42 +0800

    show s3 in aws route display component

--- src/components/aws/AwsRoute.vue
+++ src/components/aws/AwsRoute.vue
@@ -3,5 +3,6 @@
 <template>
     <router-link to="/account" page-path="">Account</router-link> |
     <router-link to="/parameter" page-path="">Parameter</router-link> |
-    <router-link to="/ecs" page-path="">Ecs Service</router-link>
+    <router-link to="/ecs" page-path="">Ecs Service</router-link> |
+    <router-link to="/s3" page-path="">S3 Bucket</router-link>
 </template>
