13e218a35a8792e9b5d47cf8d04aae3a5ee9d438
futugyou <17871902+futugyou@users.noreply.github.com>
2024-08-25 21:30:52 +0800

    update resource detail

--- src/components/infr-project/resource/Detail.vue
+++ src/components/infr-project/resource/Detail.vue
@@ -46,40 +46,40 @@ fetchData()
     <v-sheet class="d-flex flex-column align-center" height="100%">
         <Spinners v-if="isLoading"></Spinners>
         <v-card title="Resource Information" v-if="!isLoading" class="d-flex flex-column align-center">
-            <v-card-text v-if="resource" class="text-truncate">
-                <div class="d-flex ma-1 text-body-1 ga-3">
-                    <div class="font-weight-bold flex-1-1 pa-3">ID</div>
-                    <div class="pa-3">{{ resource.id }}</div>
+            <v-card-text v-if="resource" class="d-flex flex-column ga-3 text-truncate">
+                <p class="text-h4 font-weight-black">{{ resource.name }}</p>
+                <v-sheet class="d-flex flex-wrap ga-2">
+                    <v-chip>
+                        <strong>{{ resource.type }}</strong>&nbsp;
+                    </v-chip>
+                    <v-chip>
+                        <strong>{{ resource.is_deleted ? 'deleted' : 'activated' }}</strong>&nbsp;
+                    </v-chip>
+                    <v-chip v-for="tag in resource.tags">
+                        <strong>{{ tag }}</strong>&nbsp;
+                    </v-chip>
+                </v-sheet>
+
+                <v-divider></v-divider>
+
+                <v-img :aspect-ratio="1" :src="resource.data" v-if="resource.type == 'DrawIO'">
+                    <template v-slot:error>
+                        <v-sheet>
+                            <span class="text-body-1 word-break">{{ resource.data }}</span>
+                        </v-sheet>
+                    </template>
+                </v-img>
+                <div v-if="resource.type != 'DrawIO'">
+                    <span class="text-body-1 word-break">{{ resource.data }}</span>
                 </div>
-                <div class="d-flex ma-1 text-body-1 ga-3">
-                    <div class="font-weight-bold flex-1-1 pa-3">Name</div>
-                    <div class="pa-3">{{ resource.name }}</div>
-                </div>
-                <div class="d-flex ma-1 text-body-1 ga-3 ">
-                    <div class="font-weight-bold flex-1-1 pa-3">Type</div>
-                    <div class="pa-3">{{ resource.type }}</div>
-                </div>
-                <div class="d-flex ma-1 text-body-1 ga-3">
-                    <div class="font-weight-bold flex-1-1 pa-3">Data</div>
-                    <div class="pa-3">{{ resource.data }}</div>
-                </div>
-                <div class="d-flex ma-1 text-body-1 ga-3">
-                    <div class="font-weight-bold flex-1-1 pa-3">Deleted</div>
-                    <div class="pa-3">{{ resource.is_deleted }}</div>
-                </div>
-                <div class="d-flex ma-1 text-body-1 ga-3">
-                    <div class="font-weight-bold flex-1-1 pa-3">CreatedAt</div>
-                    <div class="pa-3">
-                        {{ useTimeFormat(resource.created_at) }}
-                    </div>
-                </div>
-                <div class="d-flex ma-1 text-body-1 ga-3">
-                    <div class="font-weight-bold flex-1-1 pa-3">UpdatedAt</div>
-                    <div class="pa-3">
-                        {{ useTimeFormat(resource.updated_at) }}
-                    </div>
-                </div>
-            </v-card-text> 
-        </v-card> 
+
+                <v-divider></v-divider>
+
+                <v-chip-group >
+                    <v-chip>CreatedAt: {{ useTimeFormat(resource.created_at) }}</v-chip>
+                    <v-chip>UpdatedAt: {{ useTimeFormat(resource.updated_at) }}</v-chip>
+                </v-chip-group>
+            </v-card-text>
+        </v-card>
     </v-sheet>
 </template>
