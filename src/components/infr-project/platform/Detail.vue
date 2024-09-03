e8ccb89dd9d9435a6a9bed81bf7b4df5eb60fd27
futugyou <17871902+futugyou@users.noreply.github.com>
2024-09-03 20:24:33 +0800

    show platform project webhooks

--- src/components/infr-project/platform/Detail.vue
+++ src/components/infr-project/platform/Detail.vue
@@ -88,17 +88,16 @@ fetchData()
 
             <v-tabs-window-item value="two" v-if="detail != undefined && detail.projects != undefined">
                 <v-row class="pa-3">
-                    <v-col v-for="project in detail.projects" :key="project.id" cols="12" md="3">
-                        <v-card v-if="!isLoading" class="d-flex flex-column pa-4" hover>
+                    <v-col v-for="project in detail.projects" :key="project.id" cols="12" md="4">
+                        <v-card v-if="!isLoading" class="d-flex flex-column pa-2" hover>
                             <template v-slot:title>Name: {{ project.name }} </template>
                             <template v-slot:subtitle>ID: {{ project.id }} </template>
 
                             <template v-slot:append>
                                 <a :href="project.url" target="_blank">
                                     <v-hover>
-                                        <template v-slot:default="{ isHovering, props }">
-                                            <v-icon icon="md:open_in_new" v-bind="props"
-                                                :color="isHovering ? '#75FBFD' : undefined"></v-icon>
+                                        <template v-slot:default="{ props }">
+                                            <v-icon icon="md:open_in_new" v-bind="props"></v-icon>
                                         </template>
                                     </v-hover>
                                 </a>
@@ -113,6 +112,29 @@ fetchData()
                                     <span class="text-subtitle-2">{{ value }}</span>
                                 </v-list-item>
                             </v-list>
+
+                            <v-expansion-panels>
+                                <v-expansion-panel v-for="webhook in project.webhooks" :key="webhook.name"
+                                    :title="webhook.name">
+                                    <v-expansion-panel-text>
+                                        <v-list-item title="State" :subtitle="webhook.state"></v-list-item>
+                                        <v-list-item title="Activate"
+                                            :subtitle="webhook.activate ? 'Yes' : 'No'"></v-list-item>
+                                        <v-list-item title="Url" :subtitle="webhook.url"></v-list-item>
+                                        <v-list-group>
+                                            <template v-slot:activator="{ props }">
+                                                <v-list-item v-bind="props" title="Property"></v-list-item>
+                                            </template>
+                                            <v-list-item v-for="(v, k) in webhook.property">
+                                                <span class="text-subtitle-1 mr-1">key:</span>
+                                                <span class="text-subtitle-2 mr-3">{{ v }}</span>
+                                                <span class="text-subtitle-1 mr-1">value:</span>
+                                                <span class="text-subtitle-2">{{ k }}</span>
+                                            </v-list-item>
+                                        </v-list-group>
+                                    </v-expansion-panel-text>
+                                </v-expansion-panel>
+                            </v-expansion-panels>
                         </v-card>
                     </v-col>
                 </v-row>
