dd3e765ed00507965b17cb33a7816c4fa6e38c26
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-09-05 12:46:50 +0000

    update spinners width

--- src/common/Spinners.vue
+++ src/common/Spinners.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="text-center">
+    <div class="text-center w-100">
         <v-progress-circular color="red" indeterminate :size="size"></v-progress-circular>
     </div>
 </template>  
