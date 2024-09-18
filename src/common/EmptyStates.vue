40cd64e0301bebac494f0a2eee47cb495633cf2f
futugyou <17871902+futugyou@users.noreply.github.com>
2024-09-18 11:16:37 +0800

    update empty state component

--- src/common/EmptyStates.vue
+++ src/common/EmptyStates.vue
@@ -1,5 +1,16 @@
+<script lang="ts" setup>
+</script>
+
 <template>
     <v-empty-state icon="md:search"
         text="Try adjusting your search terms or filters. Sometimes less specific terms or broader queries can help you find what you're looking for."
-        title="We couldn't find a match."></v-empty-state>
+        title="We couldn't find a match.">
+        <template v-if="$slots.default" v-slot:actions>
+            <v-spacer></v-spacer>
+
+            <slot></slot>
+
+            <v-spacer></v-spacer>
+        </template>
+    </v-empty-state>
 </template>
