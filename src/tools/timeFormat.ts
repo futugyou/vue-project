863cf08224450fc48493a019d8ec302f4a22a4d0
futugyou <17871902+futugyou@users.noreply.github.com>
2025-02-18 09:56:38 +0000

    Updated time format added data validate

--- src/tools/timeFormat.ts
+++ src/tools/timeFormat.ts
@@ -2,6 +2,9 @@ import moment from 'moment'
 
 export const timeFormat = (input: moment.MomentInput): string => {
     var day = moment(input)
+    if (!day.isValid()) {
+        return "-"
+    }
     if (day.year() == 1 && day.month() == 0 && day.day() == 1) {
         return "-"
     }
