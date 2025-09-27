a62d75f94dac2830066a3611fc4d4e53fb05b99e
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-27 17:21:15 +0000

    Added grafana sourcemap uploader

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import { visualizer } from 'rollup-plugin-visualizer'
+import faroUploader from '@grafana/faro-rollup-plugin'
+
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -34,6 +37,15 @@ export default defineConfig({
       promiseImportName: i => `__tla_${i}`,
     }),
     visualizer({ open: true, filename: 'dist/stats.html' }),
+    faroUploader({
+      appName: env.FARO_APP_NAME,
+      endpoint: env.FARO_ENDPOINT,
+      appId: env.FARO_APP_ID,
+      stackId: env.FARO_STACK_ID,
+      verbose: true,
+      apiKey: env.FARO_API_KEY,
+      gzipContents: true,
+    })
   ],
   resolve: {
     alias: {
